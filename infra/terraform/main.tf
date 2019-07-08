19d872cb798a73960a7d946e1edba63c89980c5c
Sergey Ovcharenko <sv@sv-it.ru>
2019-07-08 22:22:54 +0200

    Add docker-compose yaml

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -49,5 +49,9 @@ resource "google_compute_instance" "instance" {
     inline = ["curl -fsSL https://get.docker.com -o get-docker.sh", "sudo sh get-docker.sh", "sudo usermod -aG docker $USER"]
   }
 
+  provisioner "remote-exec" {
+    inline = ["sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose", "sudo chmod +x /usr/local/bin/docker-compose"]
+  }
+
 }
 
